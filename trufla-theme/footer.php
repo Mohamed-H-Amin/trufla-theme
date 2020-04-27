e0d2264ef1704d7bfd57cd0a7ae167f69c9cdf54
Mohamed-H-Amin <eng.aminld@gmail.com>
2020-04-27 22:03:41 +0200

    semi-final v0.1

--- trufla-theme/footer.php
+++ trufla-theme/footer.php
@@ -12,17 +12,24 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div>
+			<a href="http://validator.w3.org/check/referer" title="Check the validity of this site&#8217;s HTML" class="zen-validate-html">HTML</a>
+			<a href="http://jigsaw.w3.org/css-validator/check/referer" title="Check the validity of this site&#8217;s CSS" class="zen-validate-css">CSS</a>
+			<a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" title="View the Creative Commons license of this site: Attribution-NonCommercial-ShareAlike." class="zen-license">CC</a>
+			<a href="http://mezzoblue.com/zengarden/faq/#aaa" title="Read about the accessibility of this site" class="zen-accessibility">A11y</a>
+			<a href="https://github.com/mezzoblue/csszengarden.com" title="Fork this site on Github" class="zen-github">GH</a>
+		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'trufla-theme' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://www.trufla.com/', 'trufla-theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'trufla-theme' ), 'WordPress' );
+				printf( esc_html__( 'Proudly powered by %s', 'trufla-theme' ), 'Trufla Company' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'trufla-theme' ), 'trufla-theme', '<a href="http://eng.aminld@gmail.com">Mohamed Amin</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'trufla-theme' ), 'trufla-theme', '<a href="mailto:eng.aminld@gmail.com">Mohamed Amin</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
